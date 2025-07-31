4932048f4c7d2fcbcfcc35b7873edd8317922ad3
mariusz7b1 <35932429+mariusz7b1@users.noreply.github.com>
2025-07-31 14:14:00 +0200

    scriptblok

---
+++ Dzien2/Przyklady/06-ScriptBlok/03_ScriptBlock.ps1
@@ -0,0 +1,42 @@
+﻿# Przykład 1
+$code = { Get-Date }
+& $code
+$code.Invoke()
+
+# Przykład 2
+function Invoke-WithLogging {
+    param (
+        [ScriptBlock]$Script
+    )
+    Write-Host "Start"
+    & $Script
+    Write-Host "End"
+}
+Invoke-WithLogging -Script { Get-Process | Select-Object -First 3 }
+
+# Przykład 3
+$script = {
+    param($x, $y)
+    return $x + $y
+}
+$script.Invoke(5, 7)
+
+# Przykład 4
+Get-Service | Where-Object { $_.Status -eq 'Running' }
+1..5 | ForEach-Object { $_ * $_ }
+
+# Przykład 5
+function Process-List {
+    param(
+        [int[]]$Numbers,
+        [ScriptBlock]$Transform
+    )
+    foreach ($n in $Numbers) {
+        & $Transform.Invoke($n)
+    }
+}
+Process-List -Numbers @(1, 2, 3, 4) -Transform { param($x) "$x^2 = $($x*$x)" }
+
+# Przykład 6
+$multiply = { param($a, $b) $a * $b }
+& $multiply 3 4
---
+++ Dzien2/Przyklady/06-ScriptBlok/04_WykonajScriptBlock_z_Pliku.ps1
@@ -0,0 +1,8 @@
+﻿# Wczytanie zawartości z pliku tekstowego
+$codeFromFile = Get-Content -Path ".\scriptblock_code.txt" -Raw
+
+# Konwersja tekstu do ScriptBlock
+$scriptBlock = [ScriptBlock]::Create($codeFromFile)
+
+# Uruchomienie ScriptBlocka z parametrem
+$scriptBlock.Invoke("PowerShell User")
